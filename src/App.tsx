b9de77803b02ccd6e61c4319a8010a0ee84154f8
lebang <lebang271206@gmail.com>
2026-03-10 14:58:26 +0700

    onFinish

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
-import { Button, Layout } from "antd";
-import { Content, Footer, Header } from "antd/es/layout/layout";
+import { Layout } from "antd";
+import { Form, Input, Button } from "antd";
 
+const { Header, Content, Footer } = Layout;
 function App() {
+  //
+  const onFinish = (values: any) => {
+    console.log("onFinish");
+
+    console.log(values);
+  };
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -38,14 +45,20 @@ function App() {
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
-        <Button type="primary">Click me</Button>
-        <Button type="default">Click me</Button>
-        <Button type="dashed">Click me</Button>
-        <Button type="link">Click me</Button>
-        <Button type="text">Click me</Button>
         <Layout>
-          <Header style={{ color: "white"}}>Header</Header>
-          <Content style={{ padding: 20}}>Content</Content>
+          <Header style={{ color: "white" }}>Header</Header>
+          <Content style={{ padding: 20 }}>
+            <Form onFinish={onFinish}>
+              <Form.Item label="Username" name="username">
+                <Input placeholder="username" />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit" type="primary">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </Content>
           <Footer>Footer</Footer>
         </Layout>
       </div>
